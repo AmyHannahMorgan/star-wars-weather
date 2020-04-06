5342a0e4e87d2a64dc332888182f76d032b909d7
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-04-06 23:16:01 +0100

    +getPlanet api endpoint

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const port = process.env.PORT || 66;
 
 app.use(express.static(`${__dirname}/static`));
 
+app.get('/api/getPlanet/', (req, res) => {
+    let temp = req.query.temp;
+    let lIndex = req.query.lIndex;
+    let humid = req.query.humid;
+    let clouds = req.query.clouds;
+})
+
 app.listen(port);
 
 console.log(`listening on port: ${port}`);

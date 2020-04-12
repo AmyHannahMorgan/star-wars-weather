c91d6af8fd17b811af15ac7caf132c5332d57e3c
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-04-12 21:58:04 +0100

    +display local weather stats to user

--- static/assets/js/getWeather.js
+++ static/assets/js/getWeather.js
@@ -1,5 +1,6 @@
 const backgroundImage = document.querySelector('#backgroundImage');
 const statement = document.querySelector('.statement');
+const weatherDisplay = document.querySelector('.locationWeather');
 const apiAddress = `${window.location.origin}/api`;
 const clientLocation = navigator.geolocation;
 
@@ -31,6 +32,13 @@ error => {
 function showResults(result) {
     statement.querySelector('h1').innerText = result.planet.name;
 
+    let values = getRainAndCloudFunction(result.weather.rainAmount, result.weather.cloudCover);
+
+    weatherDisplay.querySelector('#temp').querySelector('h3').innerHTML = `${result.weather.temp}&deg;C`;
+    weatherDisplay.querySelector('#humid').querySelector('h3').innerHTML = `${result.weather.humidity}%`;
+    weatherDisplay.querySelector('#cloud').querySelector('h3').innerHTML = `${values.clouds}`;
+    weatherDisplay.querySelector('#rain').querySelector('h3').innerHTML = `${values.rain}`;
+    
     let resultBG = new Image();
     resultBG.src = `/assets/images/planet backgrounds/${result.planet.name}.png`;
 
@@ -38,4 +46,27 @@ function showResults(result) {
         backgroundImage.src = resultBG.src;
         statement.parentNode.classList.add('active');
     })
+}
+
+function getRainAndCloudFunction(rainValue, cloudValue) {
+    let cloudTerm
+    let rainTerm
+
+    if(cloudValue < 3) cloudTerm = 'Clear';
+    else if(cloudValue > 2 && cloudValue < 6) cloudTerm = 'Partially Cloudy';
+    else if(cloudValue > 5 && cloudValue < 8) cloudTerm = 'Cloudy';
+    else cloudTerm = 'Very Cloudy';
+
+    if(rainValue === 0) rainTerm = 'None';
+    else if(rainValue < 4) {
+        if(cloudValue > 8) rainTerm = 'Light Rain'
+        else if(cloudValue > 6) rainTerm = 'Occasional Showers';
+        else rainTerm = 'Isolated Showers';
+    }
+    else rainTerm = 'Rain'
+
+    return {
+        clouds: cloudTerm,
+        rain: rainTerm
+    };
 }
